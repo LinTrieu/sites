394f260c33dfcbbb69fb22978666ff85e7b849d7
Lin Trieu <linnatrieu@gmail.com>
2020-08-22 21:18:19 +0100

    Completes tutorial

--- links/tests/Feature/SubmitLinksTest.php
+++ links/tests/Feature/SubmitLinksTest.php
@@ -99,6 +99,19 @@ class SubmitLinksTest extends TestCase
         $this->fail('Max length should trigger a ValidationException');
     }
 
-//    /** @test */
-//    function max_length_succeeds_when_under_max() {}
-}
+    /** @test */
+    function max_length_succeeds_when_under_max()
+    {
+        $url = 'http://';
+        $url .= str_repeat('a', 255 - strlen($url));
+
+        $data = [
+            'title' => str_repeat('a', 255),
+            'url' => $url,
+            'description' => str_repeat('a', 255),
+        ];
+
+        $this->post('/submit', $data);
+
+        $this->assertDatabaseHas('links', $data);
+    }}
